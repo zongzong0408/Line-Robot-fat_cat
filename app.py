6d8d7a3526cb7add130c3d2ec6d5e24d8646efc0
zongzong <zongzongchu0408@gmail.com>
2022-09-27 14:29:43 +0800

    2022/09/27/02:29PM

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ from linebot.models import *
 
 import os
 
+API = "1z/vjWAX0dcEXGP29xQ43Xcr60RHlQjjEDKWNcAONdzwnTYFabgHoJX1ZJC6EDZ04QLc6Azf2HxnQgbGJstU2n9LSMZf6s1iGSY7enqKeo4CZSpV72zaU0VdGTdTD1kRl9vp+Utti3jCIuW+iUgATAdB04t89/1O/w1cDnyilFU="
+SECRET = "bf22c97b07a9cc7e3134b23e52c21a58"
+USER_ID = "Ued51038c26c9278c4789676bfe2eb215"
+
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi("pS6Govkybu0GUGdwX/aUcLC6ZY7XCYEoi8T/oIDeglIlSPKHhTBslxWnWIIpPfHC4QLc6Azf2HxnQgbGJstU2n9LSMZf6s1iGSY7enqKeo4FoifRB1FGYufO8o78vZ+s2yoENt2YccBTt5lkdZyRtAdB04t89/1O/w1cDnyilFU=")
+line_bot_api = LineBotApi(API)
 # Channel Secret
-handler = WebhookHandler("bf22c97b07a9cc7e3134b23e52c21a58")
+handler = WebhookHandler(SECRET)
 # User ID
-line_bot_api.push_message("Ued51038c26c9278c4789676bfe2eb215", TextSendMessage(text =  "Bot Start"))
+line_bot_api.push_message(USER_ID, TextSendMessage(text = "Line Bot 'fat_cat' is starting."))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -20,24 +24,30 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
- 
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    body = request.get_data(as_text = True)
+
+    app.logger.info("Request body : " + body)
 
     # handle webhook body
     try:
+
         handler.handle(body, signature)
+
     except InvalidSignatureError:
+
         abort(400)
 
     return 'OK'
 
  
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
+# 訊息傳遞區塊
+"""
+    基本上程式編輯都在這個 function
+"""
+@handler.add(MessageEvent, message = TextMessage)
 def handle_message(event):
+    
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
 
@@ -46,4 +56,4 @@ def handle_message(event):
 """
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host = '0.0.0.0', port = port)
